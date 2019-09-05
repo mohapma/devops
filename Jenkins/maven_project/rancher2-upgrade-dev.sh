21f6ec077a69b995cb9f163a4f68339215a5ddff
mohapma <manas.mohapatra@tecnotree.com>
2019-09-05 11:17:04 +0530

    updated

--- Jenkins/maven_project/rancher2-upgrade-dev.sh
+++ Jenkins/maven_project/rancher2-upgrade-dev.sh
@@ -2,14 +2,14 @@
 #!/bin/sh
 
 IMAGE_TAG=${1:-"latest"}
-IMAGE_NAME=${2:-"dev-docker-registry.tecnotree.com/ngb/ngbui"}
+IMAGE_NAME=${2:-"dev-docker-registry.tecnotree.com/digital-accelerator/management/dap-management-ui"}
 
 BEARER_TOKEN="token-8b9h6:46kpbwxnc2pdh44kwqpvhv4sddmwhxstcs7nxddksk75hksxtmzb62"       ##sandeep's token##
 RANCHER_SERVER="https://rancher2.tecnotree.com/v3"
-PROJECT="c-2cglh:p-46925"       ##dcbs-integration##
-NAMESPACE="dcbs-apps"
-DEPLOYMENT="dcbs-webui"
-POD="dcbs-webui"
+PROJECT="c-2cglh:p-znzw4"       ##digital-accelerator-platform##
+NAMESPACE="dap"
+DEPLOYMENT="dap-management"
+POD="dap-management-ui"
 
 #Download Rancher v2 CLI
 wget https://releases.rancher.com/cli2/v2.2.0/rancher-linux-amd64-v2.2.0.tar.gz  -O - | tar -zx  -C . --strip-components=2
